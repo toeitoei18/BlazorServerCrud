cd63d50ae05891dfcc365747eceb85ace22b5955
toeitoei18 <hs4toei2544@gmail.com>
2024-09-12 14:15:31 +0700

    updat

--- Services/IVideoGameService.cs
+++ Services/IVideoGameService.cs
@@ -5,6 +5,10 @@ namespace BlazorServerCrudDotnet8.Services
     public interface IVideoGameService 
     {
         Task<List<VideoGame>> GetAllGamesAsync();
+        Task<VideoGame> GetGameByIdAsync(int id);
+        Task AddGameAsync(VideoGame game);
+        Task UpdeteGameAsync(VideoGame game, int id);
+        Task DeleteGameAsync(int id);
         
 
     }
--- Services/VideoGameService.cs
+++ Services/VideoGameService.cs
@@ -12,10 +12,46 @@ namespace BlazorServerCrudDotnet8.Services
         {
             _context = context;
         }
+
+        public Task AddGameAsync(VideoGame game)
+        {
+            _context.VideoGames.Add(game);
+            return _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteGameAsync(int id)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+            if (game != null)
+            {
+                _context.VideoGames.Remove(game);
+                await _context.SaveChangesAsync();
+            }
+        }
+
         public async Task<List<VideoGame>> GetAllGamesAsync()
         {
             var result = await _context.VideoGames.ToListAsync();
             return result;
+        }
+
+        public async Task<VideoGame> GetGameByIdAsync(int id)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+            return game;
+        }
+
+        public async Task UpdeteGameAsync(VideoGame game, int id)
+        {
+            var dbgame = await _context.VideoGames.FindAsync(id);
+            if (dbgame != null)
+            {
+                dbgame.Title = game.Title;
+                dbgame.Publisher = game.Publisher;
+                dbgame.ReleaseYear = game.ReleaseYear;
+                
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
